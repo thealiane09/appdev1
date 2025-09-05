a4e21f52c9306aeb85275c96447a909cd56b9f95
Yummii09 <inalealtia15@gmail.com>
2025-09-05 23:28:35 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,11 @@
+var name = "Thea Gonzales Inale";
+var age = 20;
+age = 21;
+console.log("Using var:", name, age);
+
+let nickname = "Thea";
+nickname = "Altia";
+console.log("Using let:", nickname);
+
+const birthYear = 2005;
+console.log("Using const:", birthYear);
