5529b40e903ddf24b56bcdb7f17cd19cb2493ff5
Yummii09 <inalealtia15@gmail.com>
2025-09-05 23:29:18 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,8 @@
+const sayHi = () => console.log("Hi!");
+sayHi();
+
+const greet = (name) => "Hello, " + name;
+console.log(greet("Thea"));
+
+const square = n => n * n;
+console.log("Square of 7:", square(7));
