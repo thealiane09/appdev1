524fbcad78fd253cc3713a5af473dc45f8c280ff
Yummii09 <inalealtia15@gmail.com>
2025-09-17 00:17:36 +0800

    Done creating and nesting components

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,14 @@
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <MyButton />
+    </div>
+  );
+}
