d4cdece86084dbcc7803f672b5f1c2c33eb20815
Yummii09 <inalealtia15@gmail.com>
2025-09-17 01:03:38 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,3 +1,5 @@
+import './App.css';
+
 function MyButton() {
   return (
     <button className="my-button">I'm a button</button>
@@ -10,9 +12,31 @@ function AboutPage() {
       <h1 className="page-title">About</h1>
       <p className="about-text">
         Hello there.<br />How do you do?
-        </p>
-        <img className="avatar"
-      scr="https://via.placeholder.com/100" alt="Avatar" />
+      </p>
+      <img className="avatar" src="https://via.placeholder.com/100" alt="Avatar" />
+    </>
+  );
+}
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   );
 }
@@ -23,6 +47,7 @@ export default function App() {
       <h1>Hello, React!</h1>
       <MyButton />
       <AboutPage />
+      <Profile />
     </div>
   );
 }
