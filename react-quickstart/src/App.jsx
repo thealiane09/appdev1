d491adbe81a01d28a61d697e5bd669c6a9764df5
Yummii09 <inalealtia15@gmail.com>
2025-09-19 20:25:00 +0800

    Done rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -49,6 +49,29 @@ function LoginForm() {
   return <h2>Please log in to continue.</h2>;
 }
 
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 export default function App() {
   const isLoggedIn = true;
 
@@ -64,6 +87,8 @@ export default function App() {
       ) : (
         <LoginForm />
       )}
+
+      <ShoppingList />
     </div>
   );
 }
