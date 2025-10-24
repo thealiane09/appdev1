6a98e3e3dba6ecbff3f63e13cf96bbce3e834413
Yummii09 <inalealtia15@gmail.com>
2025-10-24 21:44:51 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,10 +1,13 @@
 import Gallery from './Gallery.jsx';
+import TodoList from './TodoList.jsx'; 
 
 export default function App() {
   return (
     <div>
       <h1>Welcome to My React App</h1>
       <Gallery />
+      <hr />
+      <TodoList /> 
     </div>
   );
 }
