c494327f62d1036e6f693635b52e3d4ac8aa4b59
Yummii09 <inalealtia15@gmail.com>
2025-11-23 23:47:24 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,9 +1,12 @@
 export default function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+
   return (
     <img
       className="avatar"
-      src="https://i.imgur.com/7vQD0fPs.jpg"
-      alt="Gregorio Y. Zara"
+      src={avatar}
+      alt={description}
     />
   );
 }
