9ebd64f6b98e9630d0ba4e3f3b75e1bfc82cc7e8
Yummii09 <inalealtia15@gmail.com>
2025-11-24 11:00:27 +0800

    Done with converting HTML to JSX

---
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,10 @@
+import ConvertingHtmlToJsx from "./ConvertingHtmlToJsx";
+
+export default function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Section 1: Converting HTML to JSX</h1>
+      <ConvertingHtmlToJsx />
+    </div>
+  );
+}
